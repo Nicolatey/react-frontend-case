fd2b25cc926058862c732ad03942c04d99460830
nsluis9446 <nsluis9446@student.landstede.nl>
2021-11-18 08:21:43 +0100

    created Navbar component

---
+++ frontend-case/src/UI/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div>
+      <img src={"https://socialbrothers.nl/wp-content/themes/social_brothers/assets/SBlogo.svg"} alt=""></img>
+      <h1>Navbar</h1>
+    </div>
+  );
+}
+ 
+export default Navbar;
